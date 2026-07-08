96a7e39d16821d9f34a145164e3d65093430a9f9
Yaroslav <romashkoyp@gmail.com>
2026-07-08 17:41:57 +0300

    preserve form values if blog creation failed

--- app/actions/blogs.ts
+++ app/actions/blogs.ts
@@ -5,7 +5,7 @@ import { revalidatePath } from "next/cache"
 import { addBlog, likeBlog } from "../services/blogs"
 import { auth } from "@/auth"
 
-export const createBlog = async (formData: FormData) => {
+export const createBlog = async (prevState: any, formData: FormData) => {
   const session = await auth()
   if (!session) {
     redirect("/login")
@@ -14,6 +14,22 @@ export const createBlog = async (formData: FormData) => {
   const author = formData.get("author") as string
   const title = formData.get("title") as string
   const url = formData.get("url") as string
+
+  const errors: { [key: string]: string } = {}
+  if (!title || title.length < 5) {
+    errors.title = "Title must be at least 5 characters"
+  }
+  if (!author || author.length < 5) {
+    errors.author = "Author must be at least 5 characters"
+  }
+  if (!url || url.length < 5) {
+    errors.url = "URL must be at least 5 characters"
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors, values: { title, author, url, content } }
+  }
+
   await addBlog(content, author, title, url)
   revalidatePath("/blogs")
   redirect("/blogs")
--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -1,32 +1,55 @@
-import { createBlog } from "../../actions/blogs" 
+"use client"
+
+import { useActionState } from "react"
+import { createBlog } from "../../actions/blogs"
+
+const initialState = {
+  errors: {},
+  values: {
+    title: "",
+    author: "",
+    content: "",
+    url: ""
+  }
+}
 
 const NewBlog = () => {
+  const [state, formAction] = useActionState(createBlog, initialState)
+
   return (
     <div>
       <h2>Create a new blog post</h2>
-      <form action={createBlog}>
+      <form action={formAction}>
         <div>
           <label>
             Title
-            <input type="text" name="title" required />
+            <input type="text" name="title" defaultValue={state?.values?.title} required />
+            {/* @ts-ignore */}
+            {state.errors.title && <p style={{ color: "red" }}>{state.errors.title}</p>}
           </label>
         </div>
         <div>
           <label>
             Author
-            <input type="text" name="author" required />
+            <input type="text" name="author" defaultValue={state?.values?.author} required />
+            {/* @ts-ignore */}
+            {state.errors.author && <p style={{ color: "red" }}>{state.errors.author}</p>}
           </label>
         </div>
         <div>
           <label>
             Content
-            <input type="text" name="content" required />
+            <input type="text" name="content" defaultValue={state?.values?.content} />
+            {/* @ts-ignore */}
+            {state.errors.content && <p style={{ color: "red" }}>{state.errors.content}</p>}
           </label>
         </div>
         <div>
           <label>
             URL
-            <input type="text" name="url" required />
+            <input type="text" name="url" defaultValue={state?.values?.url} required />
+            {/* @ts-ignore */}
+            {state.errors.url && <p style={{ color: "red" }}>{state.errors.url}</p>}
           </label>
         </div>
         <button type="submit">Create</button>
