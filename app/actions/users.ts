8fc0bea7adf3b1dc2ca15d06eb03fdeb2bf74c1e
Yaroslav <romashkoyp@gmail.com>
2026-07-10 13:44:07 +0300

    15. Validations in user registration

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -4,11 +4,36 @@ import { redirect } from "next/navigation"
 import bcrypt from "bcryptjs"
 import { db } from "@/db"
 import { users } from "@/db/schema"
+import { getUserByUsername } from "../services/users"
 
-export const registerUser = async (formData: FormData) => {
+export const registerUser = async (prevState: any, formData: FormData) => {
   const username = (formData.get("username") as string)?.trim()
   const name = (formData.get("name") as string)?.trim()
   const password = formData.get("password") as string
+  const confirmPassword = formData.get("confirmPassword") as string
+
+  const errors: { [key: string]: string } = {}
+
+  if (!username || username.length < 4) {
+    errors.username = "Username must be at least 4 characters"
+  }
+
+  const existingUser = await getUserByUsername(username)
+  if (existingUser) {
+    errors.username = "Username already exists"
+  }
+
+  if (!password || password.length < 4) {
+    errors.password = "Password must be at least 4 characters"
+  }
+
+  if (password !== confirmPassword) {
+    errors.confirmPassword = "Passwords do not match"
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors, values: { username, name, password, confirmPassword } }
+  }
 
   const passwordHash = await bcrypt.hash(password, 10)
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,27 +1,53 @@
+"use client"
+
 import Link from "next/link" // without this register page will not work
 import { registerUser } from "../actions/users"
+import { useActionState } from "react"
+
+const initialState = {
+  errors: {},
+  values: {
+    username: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+  }
+}
 
 export default function RegisterPage() {
+  const [state, formAction] = useActionState(registerUser, initialState)
   return (
     <div>
       <h2>Register</h2>
-      <form action={registerUser}>
+      <form action={formAction}>
         <div>
           <label>
             Username
-            <input type="text" name="username" required />
+            <input type="text" name="username" defaultValue={state?.values?.username} required />
+            {/* @ts-ignore */}
+            {state.errors.username && <p style={{ color: "red" }}>{state.errors.username}</p>}
           </label>
         </div>
         <div>
           <label>
             Name
-            <input type="text" name="name" required />
+            <input type="text" name="name" defaultValue={state?.values?.name} required />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input type="password" name="password" required />
+            <input type="password" name="password" defaultValue={state?.values?.password} required />
+            {/* @ts-ignore */}
+            {state.errors.password && <p style={{ color: "red" }}>{state.errors.password}</p>}
+          </label>
+        </div>
+        <div>
+          <label>
+            Confirm password
+            <input type="password" name="confirmPassword" defaultValue={state?.values?.confirmPassword} required />
+            {/* @ts-ignore */}
+            {state.errors.confirmPassword && <p style={{ color: "red" }}>{state.errors.confirmPassword}</p>}
           </label>
         </div>
         <button type="submit">Register</button>
