263c5697862ffb710022846db2d7a0551fa4a1b3
Yaroslav <romashkoyp@gmail.com>
2026-07-15 11:13:18 +0300

    adjust playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev",
+    command: "npx dotenv-cli -e .env.test -- npm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
